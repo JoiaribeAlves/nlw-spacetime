5c9c77d39bd3d912c828903438139fc9f3245648
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2023-06-11 02:11:08 -0400

    feat: lesson 2

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,66 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+import { styled } from "nativewind";
+
+import blurBg from "./src/assets/bg-blur.png";
+import Stripes from "./src/assets/stripes.svg";
+import Logo from "./src/assets/logo.svg";
+
+const StyledStripes = styled(Stripes);
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) return null;
+
   return (
-    <View className="bg-zinc-950 flex-1 items-center justify-center">
-      <Text className={"text-zinc-50 font-bold text-5xl"}>
-        Olá, mundo
-      </Text>
+    <ImageBackground
+      source={blurBg}
+      className="flex-1 items-center p-8 bg-gray-900 relative"
+      imageStyle={{ position: "absolute", left: "-100%" }}
+    >
       <StatusBar style="auto" translucent />
-    </View>
+
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <Logo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          className="rounded-full bg-green-500 px-5 py-2"
+          activeOpacity={0.7}
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-gray-200 text-center font-body leading-relaxed text-sm">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+    </ImageBackground>
   );
 }
